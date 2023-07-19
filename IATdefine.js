6894291eec6b4540cca72549cb81a9d4e3140ad8
LeonardWendering <139765348+LeonardWendering@users.noreply.github.com>
2023-07-19 08:16:48 +0200

    Update IATdefine.js

--- IATdefine.js
+++ IATdefine.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://github.com/LeonardWendering/hospitalExperiment/blob/main/base'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
