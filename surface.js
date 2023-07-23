a54e5c7b448c8382e909cd62c24ae21591c3a351
LeonardWendering <139765348+LeonardWendering@users.noreply.github.com>
2023-07-23 18:18:02 +0200

    Update surface.js

--- surface.js
+++ surface.js
@@ -38,10 +38,6 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},
 
-			//Stimulus css
-			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-		},
-		
 		base_url : {//Where are your images at?
 			image : 'https://baranan.github.io/minno-tasks/images/'
 		},
